60b312fccb44461568a915361393f595515bf2e5
Michael Dvorkin <mike@dvorkin.net>
2013-09-12 19:03:56 -0700

    Make sure Array#pick returns non-empty string of given length

--- lib/core_ext/array.rb
+++ lib/core_ext/array.rb
@@ -1,19 +1,6 @@
 class Array
   def pick(size)
-    self.select { |item| item.size == size }.sample
+    word = self.select { |item| item.size == size }.sample
+    word ||= "x" * size # Only if we miss the word in the dictionary.
   end
 end
-
-
-# line = ["Cherish", "gentle", "sky", "besides", "old", "hot", "journey?", "Miss", "pure", "skin", "and", "aged", "crystal", "wildness?", "Remember", "tranquil", "morning", "over", "wildness?"]
-# puts line.join(" ")
-# 
-# br = line.each_slice(5).each_with_index.map do |s, i|
-#   if i > 0
-#     puts s.inspect
-#     s[0] = "\n#{s[0]}" if s.size > 2
-#   end
-#   s
-# end
-# 
-# puts br.join(" ")

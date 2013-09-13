ebb5e0e986acf296c0c4da60608885e18bc3878c
Michael Dvorkin <mike@dvorkin.net>
2013-09-12 19:02:22 -0700

    Implemented decoder/evaluator

--- lib/unbelievable.rb
+++ lib/unbelievable.rb
@@ -1,55 +1,46 @@
 
-
-require 'benchmark'
-
 src = 'puts "Hello world!"'
 chars = src.unpack("C*")
-#puts chars.inspect
+puts chars.inspect
+puts sprintf("%03o" * chars.size, *chars)
 sizes = sprintf("%03o" * chars.size, *chars).unpack("C*").map{ |n| n - 45 }
 puts sizes.inspect
-
+puts '---'
 # [4, 9, 3, 4, 9, 8, 4, 9, 7, 4, 9, 6, 3, 7, 3, 3, 7, 5, 4, 4, 3, 4, 7, 8, 4, 8, 7, 4, 8, 7, 4, 8, 10, 3, 7, 3, 4, 9, 10, 4, 8, 10, 4, 9, 5, 4, 8, 7, 4, 7, 7, 3, 7, 4, 3, 7, 5]
 
-exit
-
-Benchmark.bmbm do |x|
-  # x.report { 10_000.times { src.unpack("C*").map {|c| c.to_s(8).rjust(3) }.join.tr(" 0123456", "11234567").chars.map(&:to_i) }}
-  # x.report { 10_000.times { src.unpack("C*").map {|c| c.to_s(8).rjust(3, "0") }.join.chars.map{ |n| n.to_i + 1 } }}
-  # x.report { 10_000.times { src.unpack("C*").map {|c| c.to_s(8).rjust(3, "0").chars.to_a }.flatten.map{ |n| n.to_i + 1 } }}
-
-  # x.report do
-  #   10_000.times do
-  #     chars = src.unpack("C*")
-  #     sprintf("%03o" * chars.size, *chars).tr("01234567", "12345678").chars.map(&:to_i)
-  #   end
-  # end
-  # 
-  # x.report do
-  #   10_000.times do
-  #     chars = src.unpack("C*")
-  #     sprintf("%03o" * chars.size, *chars).chars.map{ |n| n.to_i + 1 }
-  #   end
-  # end
-  # 
-  # x.report do
-  #   10_000.times do
-  #     chars = src.unpack("C*")
-  #     sprintf("%03o" * chars.size, *chars).chars.map{ |n| n.to_i + 1 }
-  #   end
-  # end
-  # 
-  x.report do
-    10_000.times do
-      chars = src.unpack("C*")
-      sprintf("%03d" * chars.size, *chars).unpack("C*").map{ |n| n - 42 }
-    end
+$code, $missing = [], []
+def method_missing(name, *args)
+  # puts "#{name}: #{args}, #{$missing.inspect}"
+  if args.empty? && $missing.any?
+    $code.concat($missing.reverse)
+    $missing.clear
   end
+  $missing << (name.to_s.size - 1).to_s
 end
 
+def Object.const_missing(name)
+  send(name.to_s.downcase.to_sym)
+end
 
+at_exit do
+  $code.concat($missing.reverse)
+  # puts $missing.reverse
+  # puts "code: " + $code.join
+  # puts "scan: " + $code.join.scan(/.../).inspect
+  # puts "pack: " + $code.join.scan(/.../).map { |o| o.bytes.map { |n| n - 2 }.pack("C*").to_i(8).chr }.join
+  eval $code.join.scan(/.../).map { |o| o.bytes.map { |n| n - 2 }.pack("C*").to_i(8).chr }.join
+end
 
-# d1 = src.unpack("C*").map { |c| c.to_s(7).rjust(3) }.join.tr(" 0123456", "11234567").chars.map(&:to_i)
-# puts d1.inspect
-# 
-# digits = src.unpack("C*").map {|c| c.to_s(7).rjust(3, "0") }.join.chars.map{ |n| n.to_i + 1 }
-# puts digits.inspect
+ooh! benchmark ten meme customers;
+redefine many impactful drivers;
+nvm! transform modern fab;
+quickly win all virtual specs;
+only fake raw GUIs;
+quickly expedite five critical schemas;
+swap thirteen foreign GUIs;
+urgently productize our leading UIs;
+nvm! implement synergized beta;
+urgently facilitate nine optimized specs;
+ooh! reinvent sixteen bold designs;
+further dig hundred labs;
+fix backend specs;
